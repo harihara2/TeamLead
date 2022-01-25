3a0fddbc3b483b4e1ce0f75d303d6ae890c9e88c
91891 <91891@LAPTOP-UHV80U0P>
2022-01-25 13:05:59 +0530

    datadriven

---
+++ src/test/java/com/drivenData/test1/PickingData.java
@@ -0,0 +1,41 @@
+package com.drivenData.test1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+public class PickingData {
+
+	public static void main(String[] args) throws Exception 
+	{
+		String path="C://Users//91891//Desktop//Data//Book1.xlsx";
+		FileInputStream file = new FileInputStream(path);
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		XSSFSheet sheet=book.getSheetAt(0);
+		int rowcount=sheet.getLastRowNum();
+		int colcount=sheet.getRow(0).getLastCellNum();
+		for(int i=1;i<=rowcount;i++)
+		{
+			XSSFRow currentrow=sheet.getRow(i);
+			for(int j=0;j<colcount;j++)
+			{
+				String value=currentrow.getCell(j).toString();
+				
+				System.out.print("    "+value);
+			}
+			System.out.println();
+		}
+		
+		
+		
+
+	}
+
+	
+}
